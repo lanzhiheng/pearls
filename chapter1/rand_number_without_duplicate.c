dcbe1fa8d37518e6e85faa520ded5a4951f26255
lanzhiheng <lanzhihengrj@gmail.com>
2019-11-29 09:21:18 +0800

    feat: Random number in multi file.

--- chapter1/rand_number_without_duplicate.c
+++ chapter1/rand_number_without_duplicate.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 #define MAX 10000000
+#define MAX_SIZE_OF_NAME 255
+#define NUM 10
 
 void shuffle(int * array, int size) {
   for (int i = size - 1; i > 0; i --) {
@@ -12,17 +14,61 @@ void shuffle(int * array, int size) {
   }
 }
 
-int main() {
-  srand(time(0));
-  FILE * p = fopen("shuffle_number_without_duplicate.seed", "w");
-  int * array = malloc(sizeof(int) * MAX);
-  for (int i = 0; i < MAX; i++) {
-    array[i] = i;
+int * random_number_list(int start, int len) {
+  int * array = malloc(sizeof(int) * len);
+  for (int i = 0; i < len; i++) {
+    array[i] = i + start;
   }
-  shuffle(array, MAX);
+  shuffle(array, len);
+  return array;
+}
 
-  for (int i = 0; i < MAX; i++) {
-    fprintf(p, "%d\n", array[i]);
+int main(int args, char ** argv) {
+  if (args != 2) abort();
+  srand(time(0));
+  char * prefix = argv[1];
+  int range = MAX / NUM;
+  char name[MAX_SIZE_OF_NAME];
+
+  for (int i = 0; i < NUM; i ++) {
+    sprintf(name, "seed/%s-%d.seed", prefix, i);
+    FILE * p = fopen(name, "w");
+    int * array = random_number_list(i * range, range);
+    for (int j = 0; j < range; j++) {
+      fprintf(p, "%d\n", array[j]);
+    }
+    fclose(p);
+    free(array);
   }
-  fclose(p);
+
+  FILE * file[NUM];
+
+  for (int i = 0; i < NUM; i++) {
+    sprintf(name, "seed/%s-%d.seed", prefix, i);
+    file[i] = fopen(name, "r");
+  }
+
+  for (int i = 0; i < NUM; i++) {
+    sprintf(name, "seed/other-%s-%d.seed", prefix, i);
+    FILE * f = fopen(name, "w");
+    char * line = NULL;
+    size_t len = 0;
+
+    int index = rand() % NUM;
+
+    for (int j = 0; j < range; j++) {
+      while (getline(&line, &len, file[index]) == -1) {
+        index = rand() % NUM;
+      }
+      int val = atoi(line);
+      fprintf(f, "%d\n", val);
+    }
+    fclose(f);
+  }
+
+  for (int i = 0; i < NUM; i++) {
+    sprintf(name, "seed/%s-%d.seed", prefix, i);
+    fclose(file[i]);
+    remove(name);
+  }
 }
