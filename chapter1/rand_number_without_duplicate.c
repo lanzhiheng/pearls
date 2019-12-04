9b7e3a35cb24096eea3b25b18735bc577c824e04
lanzhiheng <lanzhihengrj@gmail.com>
2019-12-04 18:06:17 +0800

    feat: Extra some function.

--- chapter1/rand_number_without_duplicate.c
+++ chapter1/rand_number_without_duplicate.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define MAX 1000000000
+/* #define MAX 1000000000 */
+#define MAX 10000000
 #define MAX_SIZE_OF_NAME 255
-#define NUM 100
+#define NUM 10
 
 void shuffle(int * array, int size) {
   for (int i = size - 1; i > 0; i --) {
@@ -23,13 +24,8 @@ int * random_number_list(int start, int len) {
   return array;
 }
 
-int main(int args, char ** argv) {
-  if (args != 2) abort();
-  srand(time(0));
-  char * prefix = argv[1];
-  int range = MAX / NUM;
+static void generate_files_with_shuffled_num(char * prefix, int range) {
   char name[MAX_SIZE_OF_NAME];
-
   for (int i = 0; i < NUM; i ++) {
     sprintf(name, "seed/%s-%d.seed", prefix, i);
     FILE * p = fopen(name, "w");
@@ -40,10 +36,15 @@ int main(int args, char ** argv) {
     fclose(p);
     free(array);
   }
+}
 
-  FILE * file[NUM];
+static void shuffle_all_exist_files(char * prefix, int range) {
+  char name[MAX_SIZE_OF_NAME];
 
-  for (int i = 0; i < NUM; i++) {
+  int avaiable = NUM;
+  FILE * file[avaiable]; // We will build a dynamic array
+
+  for (int i = 0; i < NUM; i++) { // Open all File with number
     sprintf(name, "seed/%s-%d.seed", prefix, i);
     file[i] = fopen(name, "r");
   }
@@ -54,11 +55,16 @@ int main(int args, char ** argv) {
     char * line = NULL;
     size_t len = 0;
 
-    int index = rand() % NUM;
+    int index = rand() % avaiable;
 
     for (int j = 0; j < range; j++) {
-      while (getline(&line, &len, file[index]) == -1) {
-        index = rand() % NUM;
+      while (getline(&line, &len, file[index]) == -1) { // current seed file has been empty
+        srand(time(0));
+        FILE * temp = file[index];
+        file[index] = file[avaiable - 1];
+        file[avaiable - 1] = temp;
+        avaiable --;
+        index = rand() % avaiable;
       }
       int val = atoi(line);
       fprintf(f, "%d\n", val);
@@ -71,4 +77,14 @@ int main(int args, char ** argv) {
     fclose(file[i]);
     remove(name);
   }
+}
+
+int main(int args, char ** argv) {
+  if (args != 2) abort();
+  srand(time(0));
+  char * prefix = argv[1];
+  int range = MAX / NUM;
+
+  generate_files_with_shuffled_num(prefix, range);
+  shuffle_all_exist_files(prefix, range);
 }
